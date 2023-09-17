a6c429e8b10e0e29f6dd5d4c6ffec8876dec6d91
ducdung1708 <ducdung17082002@gmail.com>
2023-09-17 15:27:12 +0700

    Updates

---
+++ precache-manifest.af5051089f192d2e552f92d7586caa11.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2fd638845ab8439ee4c",
+    "url": "/healthy-app/static/css/main.25ec7bc5.chunk.css"
+  },
+  {
+    "revision": "c2fd638845ab8439ee4c",
+    "url": "/healthy-app/static/js/main.58b094da.chunk.js"
+  },
+  {
+    "revision": "e7d1967063ff966baead",
+    "url": "/healthy-app/static/js/runtime~main.c4384c29.js"
+  },
+  {
+    "revision": "582d728c6570452d8dad",
+    "url": "/healthy-app/static/js/2.55733b7d.chunk.js"
+  },
+  {
+    "revision": "ae28035501139188eb868db44f9baddf",
+    "url": "/healthy-app/static/media/logo.ae280355.svg"
+  },
+  {
+    "revision": "a927376858973defc6e82badefd9f138",
+    "url": "/healthy-app/static/media/icon_menu.a9273768.svg"
+  },
+  {
+    "revision": "d7e744e7392d3cc8361b286908efc58c",
+    "url": "/healthy-app/static/media/icon_memo.d7e744e7.svg"
+  },
+  {
+    "revision": "6292260a907a99b8a716c1631e01646b",
+    "url": "/healthy-app/static/media/icon_challenge.6292260a.svg"
+  },
+  {
+    "revision": "49293ab6d682e2bc8a8855061b45569a",
+    "url": "/healthy-app/static/media/icon_info.49293ab6.svg"
+  },
+  {
+    "revision": "4684e55e2d4a9da9ccfd6498010b477e",
+    "url": "/healthy-app/static/media/icon_close.4684e55e.svg"
+  },
+  {
+    "revision": "d9db54a3d2370ff13ea03d331e0b0e79",
+    "url": "/healthy-app/static/media/morning.d9db54a3.svg"
+  },
+  {
+    "revision": "4b9b3556dee147af12b191950b17eab9",
+    "url": "/healthy-app/static/media/lunch.4b9b3556.svg"
+  },
+  {
+    "revision": "85dc86578d94ba2880fcdbb07ada8d1a",
+    "url": "/healthy-app/static/media/dinner.85dc8657.svg"
+  },
+  {
+    "revision": "880ec5e45e2f1e91d9d6e697dfafeddf",
+    "url": "/healthy-app/static/media/snack.880ec5e4.svg"
+  },
+  {
+    "revision": "008c853f281b74829eb814da6decb868",
+    "url": "/healthy-app/static/media/line.008c853f.svg"
+  },
+  {
+    "revision": "c5419a6498ef94e757316ac132f5f055",
+    "url": "/healthy-app/static/media/line.c5419a64.svg"
+  },
+  {
+    "revision": "a770db043e35ae579e8c3f9057ca4d01",
+    "url": "/healthy-app/index.html"
+  }
+];
